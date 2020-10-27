ed204caac40c03f5f8195e74987fa5410a5d704f
shanestewart4 <shanestewartguitar@gmail.com>
2020-10-27 10:38:50 -0500

    finalized drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,8 +185,11 @@ var dragTaskHandler = function(event) {
 var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
-      event.preventDefault();
+        event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
+    
+
 };
 
 var dropTaskHandler = function(event) {
@@ -204,7 +207,16 @@ var dropTaskHandler = function(event) {
     else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
     }
+
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
+};
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
 };
   
 
@@ -214,6 +226,7 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
 
 
